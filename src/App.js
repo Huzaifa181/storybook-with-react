bc790dac386dd3f85815b8fd6bdfd4e65bf37074
Huzaifa Ahmed <huzaifa.ahmed181@gmail.com>
2021-09-02 04:51:00 +0500

    Set Story Bppk

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className='app'>
+        Hello Story Book
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../src/Store';
+ReactDOM.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
