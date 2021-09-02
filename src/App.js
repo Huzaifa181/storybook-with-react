1489f33c2a66b31ae8e0e8b0b111fe569e28434d
Huzaifa Ahmed <huzaifa.ahmed181@gmail.com>
2021-09-02 05:26:33 +0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
-
+import Counter from './components/Counter'
 function App() {
   return (
     <div className='app'>
-        Hello Story Book
+        <Counter initialValue={55} backgroundColor="blue"></Counter>
     </div>
   );
 }
